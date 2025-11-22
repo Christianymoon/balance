0b39af9c7f251136afe21df7b283c5728da292f6
Christianymoon <chrisvergara7@outlook.com>
2025-11-21 19:08:23 -0600

    news api added

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
-from typing import Optional
-
+from typing import Union
 from fastapi import FastAPI
+import requests
+import os
+import dotenv
+
+dotenv.load_dotenv()
 
 app = FastAPI()
+endpoint = "https://api.apitube.io/v1/news/everything"
 
+params = {
+    "api_key": os.getenv("API_KEY"),
+    "location.name": "Mexico",
+    "category.id": "medtop:20000344",
+    "language.code": "es",
+}
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/news")
+def get_news():
+    response = requests.get(endpoint, params=params)
+    data = response.json()
+    return data['results'] # return a list of news articles dict format
