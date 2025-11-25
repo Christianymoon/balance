1e2c9be40c980ff50ee38caa693e6487ce30c282
Christian <chrisvergara7@outlook.com>
2025-11-24 20:03:13 -0600

    Handle exceptions

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from fastapi import FastAPI
 import requests
 import os
 import dotenv
+import logging
 
 dotenv.load_dotenv()
 
+logging.basicConfig(level=logging.INFO)
 
 # APITUBE API 
 
@@ -25,4 +27,11 @@ params = {
 def get_news():
     response = requests.get(endpoint, params=params)
     data = response.json()
-    return data['results'] # return a list of news articles dict format
+    try:
+        return data['results'] # return a list of news articles dict format
+    except KeyError:
+        logging.error(f"Error getting news: {data}", exc_info=True)
+        return []
+
+if __name__ == "__main__":
+    get_news()
